0847fa7c510c1ede0669e10b6a720be01b2fa163
marel20 <marelli_11@hotmail.com>
2022-07-13 01:10:14 -0300

    Creando el accordion, comments con slider  y btn up

--- js/app.js
+++ js/app.js
@@ -1,7 +1,17 @@
-const openClose = document.getElementById("open__close");
-const menuHamburguer = document.getElementById("menu__hamburguer");
+var openClose = document.getElementById("open__close");
+var menuHamburguer = document.getElementById("menu__hamburguer");
 
 openClose.addEventListener("click", () =>{
     menuHamburguer.classList.toggle("take__off");
     openClose.classList.toggle("change");
 })
+
+let btnUp = window.pageYOffset;
+window.onscroll = function() {
+    let displace = window.pageYOffset;
+    if (btnUp < displace){
+        this.document.getElementById('up').style.display = 'block';
+    } else{
+        document.getElementById('up').style.display = 'none';
+    }
+}
